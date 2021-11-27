2c18517977d22649afcccb63a366e1b690714265
BishopJohnson <BishopJohnson@users.noreply.github.com>
2021-11-27 05:06:54 -0800

    Added `define.hpp`

---
+++ define.hpp
@@ -0,0 +1,61 @@
+// Content Keys
+#define HELI_KEY "Helicopters"
+#define MARKSMEN_KEY "Marksmen"
+#define APEX_KEY "Apex"
+#define JETS_KEY "Jets"
+#define ORANGE_KEY "Orange" // Laws of War
+#define TAC_OPS_KEY "Tac Ops"
+#define TANKS_KEY "Tanks"
+#define CONTACT_KEY "Contact"
+
+// TODO: Add FIA, Gendarmerie, Syndikat, and Looters factions
+// Faction Keys
+#define NATO_KEY "NATO" // Core
+#define CSAT_KEY "CSAT" //
+#define AAF_KEY "AAF"   //
+#define CSAT_P_KEY "CSAT Pacific" // Apex (Tanoa)
+#define SPETSNAZ_KEY "Spetsnaz" // Contact (Livonia)
+#define LDF_KEY "LDF"           //
+//#defina US_KEY "US"   // RHS
+//#define RU_KEY "RU"   //
+//#define CDF_KEY "CDF" //
+
+// Map Keys
+#define ALTIS_KEY "Altis"
+#define LIVONIA_KEY "Enoch"
+#define MALDEN_KEY "Malden"
+#define STRATIS_KEY "Stratis"
+#define TANOA_KEY "Tanoa"
+#define VR_KEY "VR"
+
+// Item Keys
+#define AK_12_KEY "ak12"
+#define ASP_1_KEY "asp1"
+#define Cyrus_KEY "cyrus"
+#define FOUR_FIVE_KEY "4Five"
+#define LRR_KEY "lrr"
+#define LYNX_KEY "lynx"
+#define MAAWS_KEY "maaws"
+#define MAR_10_KEY "mar10"
+#define MK_14_KEY "mk14"
+#define MK_20_KEY "mk20"
+#define MK_200_KEY "mk200"
+#define MKI_EMR_KEY "mkiEmr"
+#define P_07_KEY "p07"
+#define P_90_KEY "p90"
+#define PCML_KEY "pcml"
+#define ROOK_KEY "rook"
+#define RPG_42_KEY "rpg42"
+#define SDAR_KEY "sdar"
+#define SPMG_KEY "spmg"
+#define STING_KEY "sting"
+#define TITAN_AA_KEY "titanAa"
+#define TITAN_AT_KEY "titanAt"
+#define TYPE_115_KEY "type115"
+#define VORONA_KEY "vorona"
+#define ZUBR_KEY "zubr"
+
+// Random Cargo Keys
+#define RAND_CARGO_COMMON_KEY "common"
+#define RAND_CARGO_UNCOMMON_KEY "uncommon"
+#define RAND_CARGO_RARE_KEY "rare"
